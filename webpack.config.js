9fe7f3ce96271797869cc19ee61286c81f4564f6
zhangliu <zhangliuge@yeah.net>
2018-12-11 21:09:06 +0800

    modify

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const uglify = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: './index.js',
+    // mode: 'development',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'source-map',
+    plugins: [
+        new uglify({sourceMap: true})
+    ],
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    }
+}
